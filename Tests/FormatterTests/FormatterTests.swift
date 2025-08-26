57718f274995a0474e371f0f8adb8158ed3f1388
Benedek Kozma <cyberbeni@gmail.com>
2025-08-26 19:11:54 +0200

    Use local timezone for iso date format too

--- Tests/FormatterTests/FormatterTests.swift
+++ Tests/FormatterTests/FormatterTests.swift
@@ -9,12 +9,11 @@ struct FormatterTests {
 
 	@Test
 	func specificDate() throws {
+		let date = try #require(Calendar.current.date(from: .init(year: 2025, month: 1, day: 2, hour: 13, minute: 45, second: 56)))
 		#if LocalizedTimestamp
-			let date = try #require(Calendar.current.date(from: .init(year: 2025, month: 1, day: 2, hour: 13, minute: 45, second: 56)))
 			#expect(Formatter.format(date: date) == "2025. 01. 02. 13:45:56")
 		#else
-			let date = Date.init(timeIntervalSince1970: 1735825556)
-			#expect(Formatter.format(date: date) == "2025-01-02 13:45:56Z")
+			#expect(Formatter.format(date: date).hasPrefix("2025-01-02 13:45:56"))
 		#endif
 	}
 }
